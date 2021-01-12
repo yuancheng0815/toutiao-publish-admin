dc07c5260d74f58d725f3bbc3852a8b881a64234
cheng yuan <yuanc0527@163.com>
2021-01-12 10:07:33 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载element组件库
+import ElementUI from 'element-ui'
+
+// 加载element组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 加载全局样式
+import './styles/index.less'
+
+// 全局注册element组件库
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
